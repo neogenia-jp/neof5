0865d0bf57e16b6eeaf9d0a26936d55e28ccaa2c
w maeda <w.maeda@neogenia.co.jp>
2013-12-05 13:28:48 +0900

    プロジェクト設定を修正

---
+++ Daifugo/WebDaifugo/Controllers/PlayroomController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebDaifugo.Controllers
+{
+    public class PlayroomController : Controller
+    {
+        //
+        // GET: /Playroom/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
