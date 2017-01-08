e8cf4fc5f1cff9c1c823e8751471367abc38a858
Wataru Maeda <w.maeda@neogenia.co.jp>
2017-01-08 12:33:25 +0900

    ホスティングの設定をjsonファイルと環境変数で変更できるようにした

--- Portal/src/Neof5WebSite/Program.cs
+++ Portal/src/Neof5WebSite/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace Neof5WebSite
 {
@@ -11,9 +12,16 @@ namespace Neof5WebSite
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("hosting.json", optional: true)
+                .AddEnvironmentVariables()  // 環境変数で上書きできるようにする
+                .Build();
+
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseConfiguration(config)
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
